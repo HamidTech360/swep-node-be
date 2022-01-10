68c2d8086b56fab1bd53040799b9a42172dfb402
HamidTech360 <hammedowolabi2001@gmail.com>
2022-01-10 01:30:22 -0800

    configuring email

--- src/config/index.js
+++ src/config/index.js
@@ -6,13 +6,15 @@ module.exports = (() => {
     return {
       DATABASE_URL: process.env.DATABASE_URL,
       PORT: process.env.PORT,
-      JWT_SECRET: process.env.JWT_SECRET
+      JWT_SECRET: process.env.JWT_SECRET,
+      EMAIL:process.env.EMAIL,
+      PASSWORD:process.env.PASSWORD
     }
   }
   return {
       DATABASE_URL: "mongodb://localhost:27017/swep-db",
       JWT_SECRET: "evruyvryeyryevruer",
-      PORT: 6000,
+      PORT: 3001,
       EMAIL:'owolabihammed2001@gmail.com',
       PASSWORD:'olalekan2019...'
     }
